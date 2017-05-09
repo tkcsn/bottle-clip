0d5f0ffa4066d75fa2b12a5889446e33fc86afb9
tkcsn <tkcsn0@gmail.com>
2017-05-09 19:18:06 +0900

    imageAPI追加

--- server.py
+++ server.py
@@ -1,35 +1,40 @@
-from bottle import route, run, request, HTTPResponse, template
+from bottle import route, run, request, HTTPResponse, template, static_file
 import json
 
 @route('/')
 def root():
     return template('show', name="root")
 
-@route('/v1/search/user')
+@route('/api/v1/users/search')
 def search():
-    query = request.query.get('query')
+    query = request.query.get('q')
     
     body = json.dumps([{"name":"Harry Potter"},{"name":"Draco Malfoy"},{"name":"Lord Voldemort"}])
     r = HTTPResponse(status=200, body=body)
     r.set_header("Content-Type", "application/json")
     return r
 
-@route('/v1/search/tag')
+@route('/api/v1/tags/search')
 def search():
-    query = request.query.get('query')
+    query = request.query.get('q')
     
     body = json.dumps([{"name":"#table"},{"name":"#desk"},{"name":"#char"}])
     r = HTTPResponse(status=200, body=body)
     r.set_header("Content-Type", "application/json")
     return r
 
-@route('/v1/search/brand')
+@route('/api/v1/brands/search')
 def search():
-    query = request.query.get('query')
+    query = request.query.get('q')
     
     body = json.dumps([{"name":"#table"},{"name":"#desk"},{"name":"#char"}])
     r = HTTPResponse(status=200, body=body)
     r.set_header("Content-Type", "application/json")
     return r
+
+@route('/imgs/<filename:path>')
+def image(filename):
+    res = static_file(filename, root='./imgs')
+    return res
 
 run(host='localhost', port=8080, debug=True, reloader=True)
